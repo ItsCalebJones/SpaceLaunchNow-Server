6c3dc2c0dd9e55d051b0c06517805a50c85c9cae
Caleb Jones <ca.jones9119@gmail.com>
2017-05-19 22:22:43 -0700

    Testing templates...x2

--- spacelaunchnow/settings.py
+++ spacelaunchnow/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'spacelaunchnow.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [PROJECT_PATH + '/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
