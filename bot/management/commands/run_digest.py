d88ce522127d31ce653462c6433b6a2accee590a
Caleb Jones <ca.jones9119@gmail.com>
2017-06-29 16:55:37 -0400

    Improvements to commands and notification bug fix.

--- bot/management/commands/run_digest.py
+++ bot/management/commands/run_digest.py
@@ -15,12 +15,18 @@ class Command(BaseCommand):
         feature_parser.add_argument('--daily', dest='daily', action='store_true')
         feature_parser.add_argument('--weekly', dest='daily', action='store_false')
         parser.add_argument('-version', dest="version", type=str)
-        parser.add_argument('-debug', dest="debug", type=bool)
+        parser.add_argument('-debug', dest="debug", type=bool, const=True, nargs='?')
         parser.set_defaults(daily=True)
 
     def handle(self, *args, **options):
         logger.info('Running Digest - Daily = %s' % options['daily'])
         debug = options['debug']
+        while debug is None:
+            response = raw_input('Continue in production mode? (Y/N) ')
+            if response == "Y":
+                debug = False
+            if response == "N":
+                debug = True
         version = options['version']
         daily_digest = DailyDigestServer(debug=debug, version=version)
         if options['daily'] is True:
