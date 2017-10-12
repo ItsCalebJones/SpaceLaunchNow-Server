253eb153e8bc0c01cb978dfe4c4f9bb8cc7fe800
Caleb Jones <ca.jones9119@gmail.com>
2017-10-12 00:01:51 -0400

    WIP

--- landing/views.py
+++ landing/views.py
@@ -5,10 +5,22 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from bot.libraries.launchlibrarysdk import LaunchLibrarySDK
+from bot.utils.deserializer import launch_json_to_model
+
+
 def index(request):
     return render(request, 'landing/index.html',)
 
 
 # Create your views here.
 def next_launch(request):
-    return render(request, 'next/next.html',)
+    launchLibrary = LaunchLibrarySDK(version='1.2.1')
+    response = launchLibrary.get_next_launch()
+    if response.status_code is 200:
+        response_json = response.json()
+        launch_data = response_json['launches']
+        launch = launch_json_to_model(launch_data[0])
+        return render(request, 'next/next.html', {'launch': launch})
+    else:
+        return render(request, 'next/next.html', )
