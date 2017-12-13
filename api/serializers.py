3d8aef18f495f8f498b34e9c37b2c4fe0bf61d7b
Caleb Jones <ca.jones9119@gmail.com>
2017-12-13 17:17:31 -0500

    Change serializer.

--- api/serializers.py
+++ api/serializers.py
@@ -8,13 +8,10 @@ class LauncherSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'url', 'name', 'agency', 'image_url', 'nation_url')
 
 
-class AgencySerializer(serializers.HyperlinkedModelSerializer):
-    launcher_list = serializers.StringRelatedField(many=True)
-    orbiter_list = serializers.StringRelatedField(many=True)
-
+class LauncherModelSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Agency
-        fields = ('url', 'agency', 'launchers', 'orbiters', 'launcher_list', 'orbiter_list', 'description', 'image_url', 'nation_url')
+        model = Launcher
+        fields = ('id', 'url', 'name', 'agency', 'image_url', 'nation_url')
 
 
 class OrbiterSerializer(serializers.HyperlinkedModelSerializer):
@@ -22,6 +19,21 @@ class OrbiterSerializer(serializers.HyperlinkedModelSerializer):
         model = Orbiter
         fields = ('id', 'url', 'name', 'agency', 'history', 'details', 'image_url', 'nation_url',
                   'wiki_link')
+
+
+class OrbiterModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Launcher
+        fields = ('id', 'url', 'name', 'agency', 'image_url', 'nation_url')
+        
+
+class AgencySerializer(serializers.HyperlinkedModelSerializer):
+    launcher_list = LauncherModelSerializer(many=True, read_only=True)
+    orbiter_list = OrbiterModelSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Agency
+        fields = ('url', 'agency', 'launchers', 'orbiters', 'launcher_list', 'orbiter_list', 'description', 'image_url', 'nation_url')
 
 
 class LauncherDetailSerializer(serializers.HyperlinkedModelSerializer):
