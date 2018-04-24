85104042494346ee416e5477121dc24c3f58bb63
Caleb Jones <ca.jones9119@gmail.com>
2018-04-24 15:54:52 -0400

    Password Reset

--- spacelaunchnow/urls.py
+++ spacelaunchnow/urls.py
@@ -42,4 +42,12 @@ urlpatterns = [
     url(r'^next/', landing_views.next_launch, name='next'),
     url(r'^launch/(?P<pk>\d+)/$', landing_views.launch_by_id, name='launch_by_id'),
     url(r'^launch/$', landing_views.launches, name='launches'),
+
+    # Changing Password
+    url('^', include('django.contrib.auth.urls')),
+    # url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    # url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    # url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     auth_views.password_reset_confirm, name='password_reset_confirm'),
+    # url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
