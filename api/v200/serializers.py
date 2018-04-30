edd0f460e0c8ffbce57764de113bcd7be1b9896a
Jacques Racagneres <jacquesrascagneres@gmail.com>
2018-04-30 01:58:57 +0100

    Switched all images to S3, added legacy field which v1 uses

--- api/v200/serializers.py
+++ api/v200/serializers.py
@@ -5,20 +5,21 @@ from rest_framework import serializers
 class LauncherModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = LauncherDetail
-        fields = ('id', 'url', 'name', 'description', 'agency', 'variant', 'image_url', 'info_url', 'wiki_url')
+        fields = ('id', 'url', 'name', 'description', 'agency', 'variant', 'legacy_image_url', 'image_url',
+                  'info_url', 'wiki_url')
 
 
 class OrbiterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Orbiter
-        fields = ('id', 'url', 'name', 'agency', 'history', 'details', 'image_url', 'nation_url',
-                  'wiki_link')
+        fields = ('id', 'url', 'name', 'agency', 'history', 'details', 'legacy_image_url', 'image_url',
+                  'legacy_nation_url', 'nation_url', 'wiki_link')
 
 
 class OrbiterModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orbiter
-        fields = ('id', 'url', 'name', 'agency', 'image_url', 'nation_url')
+        fields = ('id', 'url', 'name', 'agency', 'legacy_image_url', 'image_url', 'legacy_nation_url', 'nation_url')
 
 
 class AgencySerializer(serializers.HyperlinkedModelSerializer):
@@ -27,8 +28,9 @@ class AgencySerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Agency
-        fields = ('url', 'name', 'featured', 'launchers', 'orbiters', 'launcher_list', 'orbiter_list', 'description', 'image_url',
-                  'nation_url', 'ceo', 'founding_year', 'logo_url', 'launch_library_url', 'launch_library_id')
+        fields = ('url', 'name', 'featured', 'launchers', 'orbiters', 'launcher_list', 'orbiter_list', 'description',
+                  'legacy_image_url', 'image_url', 'legacy_nation_url', 'nation_url', 'ceo', 'founding_year', 'logo_url',
+                  'launch_library_url', 'launch_library_id')
 
 
 class LauncherDetailSerializer(serializers.HyperlinkedModelSerializer):
@@ -37,5 +39,4 @@ class LauncherDetailSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'url', 'name', 'description', 'family', 's_family', 'full_name', 'agency',
                   'variant', 'alias', 'min_stage', 'max_stage', 'length', 'diameter',
                   'launch_mass', 'leo_capacity', 'gto_capacity', 'to_thrust', 'vehicle_class',
-                  'apogee', 'vehicle_range', 'image_url', 'info_url', 'wiki_url')
-
+                  'apogee', 'vehicle_range', 'legacy_image_url', 'image_url', 'info_url', 'wiki_url')
