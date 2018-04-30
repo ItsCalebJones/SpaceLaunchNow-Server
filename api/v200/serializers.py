fdb221a2e5dadec1f95d90f6e8fc96e8d563accd
Caleb Jones <ca.jones9119@gmail.com>
2018-04-30 01:23:43 -0400

    Remove legacy image URL's from v200

--- api/v200/serializers.py
+++ api/v200/serializers.py
@@ -5,21 +5,21 @@ from rest_framework import serializers
 class LauncherModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = LauncherDetail
-        fields = ('id', 'url', 'name', 'description', 'agency', 'variant', 'legacy_image_url', 'image_url',
+        fields = ('id', 'url', 'name', 'description', 'agency', 'variant',  'image_url',
                   'info_url', 'wiki_url')
 
 
 class OrbiterSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Orbiter
-        fields = ('id', 'url', 'name', 'agency', 'history', 'details', 'legacy_image_url', 'image_url',
+        fields = ('id', 'url', 'name', 'agency', 'history', 'details', 'image_url',
                   'legacy_nation_url', 'nation_url', 'wiki_link')
 
 
 class OrbiterModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Orbiter
-        fields = ('id', 'url', 'name', 'agency', 'legacy_image_url', 'image_url', 'legacy_nation_url', 'nation_url')
+        fields = ('id', 'url', 'name', 'agency', 'image_url', 'legacy_nation_url', 'nation_url')
 
 
 class AgencySerializer(serializers.HyperlinkedModelSerializer):
@@ -39,4 +39,4 @@ class LauncherDetailSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'url', 'name', 'description', 'family', 's_family', 'full_name', 'agency',
                   'variant', 'alias', 'min_stage', 'max_stage', 'length', 'diameter',
                   'launch_mass', 'leo_capacity', 'gto_capacity', 'to_thrust', 'vehicle_class',
-                  'apogee', 'vehicle_range', 'legacy_image_url', 'image_url', 'info_url', 'wiki_url')
+                  'apogee', 'vehicle_range', 'image_url', 'info_url', 'wiki_url')
