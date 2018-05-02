9448db475b6ccd2f8fd17c10e8438c8d664f6ed7
Caleb Jones <ca.jones9119@gmail.com>
2018-05-02 16:24:04 -0400

    Update Terms of use and Privacy

--- spacelaunchnow/urls.py
+++ spacelaunchnow/urls.py
@@ -34,8 +34,10 @@ urlpatterns = [
     url(r'^next/', landing_views.next_launch, name='next'),
     url(r'^launch/(?P<pk>\d+)/$', landing_views.launch_by_id, name='launch_by_id'),
     url(r'^launch/$', landing_views.launches, name='launches'),
-    url(r'^privacy', TemplateView.as_view(template_name='web/privacy.html'), name='privacy'),
-    url(r'^tos', TemplateView.as_view(template_name='web/tos.html'), name='tos'),
+    url(r'^app/privacy', TemplateView.as_view(template_name='web/app/privacy.html'), name='privacy'),
+    url(r'^app/tos', TemplateView.as_view(template_name='web/app/tos.html'), name='tos'),
+    url(r'^site/privacy', TemplateView.as_view(template_name='web/site/privacy.html'), name='privacy'),
+    url(r'^site/tos', TemplateView.as_view(template_name='web/site/tos.html'), name='tos'),
     # Changing Password
     url('^', include('django.contrib.auth.urls')),
 ]
