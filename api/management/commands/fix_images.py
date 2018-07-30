ec8eb6bbd11b92eb653830ece87fe05cd1591174
Caleb Jones <ca.jones9119@gmail.com>
2018-07-30 15:25:19 -0400

    Attempt fix.

--- api/management/commands/fix_images.py
+++ api/management/commands/fix_images.py
@@ -16,18 +16,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for each in Launcher.objects.all():
-
             if 'cloudinary' in str(each.image_url) or 'imgur' in str(each.image_url):
-
                 each.image_url = ''
-            else:
-                print(each.image_url)
-                print(each.image_url.url)
-                if 'cloudinary' in each.image_url.url or 'imgur' in each.image_url.url:
-                    print("Removing errored image.")
-                    each.image_url = ''
             if each.image_url == '':
-                each.save()
                 result = requests.get("http://launchlibrary.net/1.3/rocket/" + str(each.id))
                 if len(result.json()['rockets']) > 0:
                     webrocket = result.json()['rockets'][0]
@@ -37,16 +28,12 @@ class Command(BaseCommand):
                         filename = webrocket['imageURL'].split('/')[-1]
                         filename, file_extension = os.path.splitext(filename)
                         clean_name = urllib.quote(urllib.quote(each.name.encode('utf8')), '')
-
                         clean_name = "%s_nation_%s" % (clean_name.lower(), datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-
                         name = "%s%s" % (str(clean_name), file_extension)
 
                         lf = tempfile.NamedTemporaryFile()
 
-
                         for block in request.iter_content(1024*8):
-
                             if not block:
                                 break
                             lf.write(block)
