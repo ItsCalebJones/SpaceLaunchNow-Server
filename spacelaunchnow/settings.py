1e29aac45004774de83ce51921e459d893c3260a
Caleb Jones <ca.jones9119@gmail.com>
2018-08-28 15:42:42 -0400

    Move database configuration to config file.

--- spacelaunchnow/settings.py
+++ spacelaunchnow/settings.py
@@ -222,12 +222,7 @@ WSGI_APPLICATION = 'spacelaunchnow.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = config.DATABASE
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
