9536ae13239d314d91d6cae590925abc3a6ad2bf
Caleb Jones <ca.jones9119@gmail.com>
2018-09-17 16:46:04 -0400

    Remove silk

--- spacelaunchnow/urls.py
+++ spacelaunchnow/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     # Changing Password
     url('^', include('django.contrib.auth.urls')),
     url(r'^signup/$', landing_views.signup, name='signup'),
-    url(r'^silk/', include('silk.urls', namespace='silk'))
+    # url(r'^silk/', include('silk.urls', namespace='silk'))
 ]
 
 handler404 = web.views.handler404
