ab93e43be0518ba1bd2dc6c3772a88c019273488
Caleb Jones <ca.jones9119@gmail.com>
2018-11-27 14:53:24 -0500

    Dev (#79)
    * Quick fix to first stage count, added GO to not be counted
    
    * Added maiden flight for Launcher Config and added first stage flight number (#69)
    
    * Make it easier to re-import booster status.
    
    * Automatically sync status/details of existing cores, create new cores as found.
    
    * Added Support for Crew Tracking and Orbiters (#76)
    
    * Added models and migrations for crew support
    
    TODO - Decide on compat for orbiter name change
    
    * Migrations tested and added serializer & endpoints
    
    * More work on serializers and data change
    
    * Models and serializers done
    
    * Normal vs Detailed Serializers
    
    * Tests Complete and Moved Status' to Configuration
    
    * Admin models moved
    
    * Fix merge issue.
    
    * Fix typo
    
    * Added astro: bio, profile_image, wiki & bio field made bigger (#78)
    
    * Added astro: bio, profile_image, wiki & bio field made bigger
    
    * Merged migrations

--- custom_storages.py
+++ custom_storages.py
@@ -13,41 +13,40 @@ class StaticStorage(S3Boto3Storage):
 
 
 class LogoStorage(S3Boto3Storage):
-
     location = settings.LOGO_LOCATION
     file_overwrite = True
 
 
 class AgencyImageStorage(S3Boto3Storage):
-
     location = settings.AGENCY_IMAGE_LOCATION
     file_overwrite = True
 
 
 class AgencyNationStorage(S3Boto3Storage):
-
     location = settings.AGENCY_NATION_LOCATION
     file_overwrite = True
 
 
 class OrbiterImageStorage(S3Boto3Storage):
-
     location = settings.ORBITER_IMAGE_LOCATION
     file_overwrite = True
 
 
 class LauncherImageStorage(S3Boto3Storage):
-
     location = settings.LAUNCHER_IMAGE_LOCATION
     file_overwrite = True
 
 
 class EventImageStorage(S3Boto3Storage):
-
     location = settings.EVENT_IMAGE_LOCATION
     file_overwrite = True
 
-class AppImageStorage(S3Boto3Storage):
 
+class AppImageStorage(S3Boto3Storage):
     location = settings.APP_IMAGE_LOCATION
+    file_overwrite = True
+
+
+class AstronautImageStorage(S3Boto3Storage):
+    location = settings.ASTRONAUT_IMAGE_LOCATION
     file_overwrite = True
