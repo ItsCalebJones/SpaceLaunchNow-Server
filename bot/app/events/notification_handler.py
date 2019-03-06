2095eec3f6e792ebfad7f9a58205bd0e301a2061
Caleb Jones <ca.jones9119@gmail.com>
2019-03-06 11:52:48 -0500

    Build data and topics.

--- bot/app/events/notification_handler.py
+++ bot/app/events/notification_handler.py
@@ -19,7 +19,30 @@ class EventNotificationHandler:
             self.DEBUG = debug
 
     def send_ten_minute_notification(self, event):
-        pass
+        data = self.build_data(event)
 
     def send_webcast_notification(self, event):
+        data = self.build_data(event)
+
+    def build_data(self, event):
+        return {
+            "type": "event",
+            "event_id": event.id,
+            "event_name": event.name,
+            "event_description": event.description,
+            "event_type_id": event.type.id,
+            "event_type_name": event.type.name,
+            "event_date": event.date.strftime("%B %d, %Y %H:%M:%S %Z"),
+            "event_location": event.location,
+            "event_news_url": event.news_url,
+        }
+
+    def build_topics(self, event):
+        if self.DEBUG:
+            topic = "'debug_v2' in topics && 'events' in topics && '%s' in topics" % event.type.name
+        else:
+            topic = "'prod_v2' in topics && 'events' in topics && '%s' in topics" % event.type.name
+        return topic
+
+    def send_notification(self, topics, data):
         pass
