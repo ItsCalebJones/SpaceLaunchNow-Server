8ef88b5dc22de23ecfb2e84da97f882e5f2cf3f3
Caleb Jones <ca.jones9119@gmail.com>
2019-03-12 08:59:49 -0400

    Send event notifications.

--- bot/management/commands/run_check_event.py
+++ bot/management/commands/run_check_event.py
@@ -5,9 +5,10 @@ from django.core.management import BaseCommand
 from celery.utils.log import get_task_logger
 
 from api.models import Events
+from bot.app.events.notification_handler import EventNotificationHandler
 from bot.app.events.twitter_handler import EventTwitterHandler
 
-logger = get_task_logger('bot')
+logger = get_task_logger('bot.events')
 
 TAG = 'Digest Server'
 
@@ -20,7 +21,9 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         logger.info('Check Events')
-        # event = Events.objects.filter(date__gte=datetime.datetime.now(tz=pytz.utc)).first()
-        # if event:
-        #     twitter = EventTwitterHandler()
-        #     twitter.send_ten_minute_tweet(event)
+        event = Events.objects.filter(date__gte=datetime.datetime.now(tz=pytz.utc)).first()
+        if event:
+            notification = EventNotificationHandler()
+            notification.send_webcast_notification(event)
+            # twitter = EventTwitterHandler()
+            # twitter.send_ten_minute_tweet(event)
