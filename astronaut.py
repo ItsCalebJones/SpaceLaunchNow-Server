6bb5ca253d132180566724e3a5ff5837a79fbece
Caleb Jones <ca.jones9119@gmail.com>
2019-04-26 10:36:14 -0400

    Migrate astronuats to new status.

---
+++ astronaut.py
@@ -0,0 +1,26 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spacelaunchnow.settings")
+django.setup()
+
+from api.models import *
+from configurations.models import *
+
+if __name__ == '__main__':
+    deceased, created = AstronautStatus.objects.get_or_create(name='Deceased')
+    retired = AstronautStatus.objects.get(name='Retired')
+    retired_with_death_date = Astronaut.objects.filter(status=retired).filter(date_of_death__isnull=False)
+    for astronaut in retired_with_death_date:
+        print("--------------------------------")
+        print(astronaut)
+        print(astronaut.status)
+        print(astronaut.date_of_death)
+        print("Migrating...")
+        astronaut.status = deceased
+        astronaut.save()
+        print(astronaut)
+        print(astronaut.status)
+        print(astronaut.date_of_death)
+        print("--------------------------------")
+        print("/n")
