1d6e7eb6c9f1e22f42b644bffa3de7a1ec130411
Caleb Jones <ca.jones9119@gmail.com>
2020-09-24 10:55:03 -0400

    Update tasks.py

--- bot/tasks.py
+++ bot/tasks.py
@@ -36,15 +36,15 @@ app = Celery()
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+    sender.add_periodic_task(10.0, test_task.s('hello'), name='add every 10')
 
     # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
+    sender.add_periodic_task(30.0, test_task.s('world'), expires=10)
 
 
 @app.task
-def test(arg):
-    print(arg)
+def test_task(arg):
+    logger.info(arg)
 
 
 @periodic_task(
