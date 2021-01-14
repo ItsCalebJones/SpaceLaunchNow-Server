b36af5ef4e1538f7cb69c92f2a6698e5d08bef95
Caleb Jones <ca.jones9119@gmail.com>
2021-01-14 10:37:08 -0500

    Move SNAPIArticle into SLN - migrate to v2

--- bot/app/sync/news_sync.py
+++ bot/app/sync/news_sync.py
@@ -6,37 +6,39 @@ import pytz
 import requests
 from goose3 import Goose
 
-from api.models import Events, Launch, Article
-from bot.models import ArticleNotification
+from api.models import Events, Launch
+from bot.models import SNAPIArticle, ArticleNotification
 
 logger = logging.getLogger('bot.digest')
 
 
 def get_news(limit=10):
-    response = requests.get(url='https://spaceflightnewsapi.net/api/v1/articles?limit=%s' % limit)
+    response = requests.get(url='https://spaceflightnewsapi.net/api/v2/articles?_limit=%s' % limit)
     if response.status_code == 200:
-        articles = response.json()['docs']
+        articles = response.json()
         logger.info("Found %s articles." % len(articles))
         for item in articles:
             save_news(item)
 
 
 def save_news(item):
-    news, news_created = Article.objects.get_or_create(id=item['_id'])
+    news, news_created = SNAPIArticle.objects.get_or_create(id=item['id'])
     record, record_created = ArticleNotification.objects.get_or_create(id=news.id, article=news)
     if news_created:
         news.title = item['title']
         news.link = item['url']
-        news.featured_image = item['featured_image']
-        news.news_site = item['news_site_long']
-        news.created_at = datetime.utcfromtimestamp(item['date_published']).replace(tzinfo=pytz.utc)
-        for event_id in item['events']:
+        news.featured_image = item['imageUrl']
+        news.news_site = item['newsSite']
+        news.created_at = datetime.strptime(item['publishedAt'][:-5], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=pytz.utc)
+        for _event in item['events']:
+            event_id = _event['id']
             try:
                 event = Events.objects.get(id=event_id)
                 news.events.add(event)
             except Events.DoesNotExist:
                 logger.error("No event found with ID %s" % event_id)
-        for launch_id in item['launches']:
+        for _launch in item['launches']:
+            launch_id = _launch['id']
             try:
                 launch = Launch.objects.get(id=launch_id)
                 news.launches.add(launch)
@@ -65,9 +67,9 @@ def save_news(item):
     else:
         if news.title != item['title']:
             news.title = item['title']
-            if (news.created_at - datetime.utcfromtimestamp(item['date_published']).replace(
+            if (news.created_at - datetime.strptime(item['publishedAt'][:-5], '%Y-%m-%dT%H:%M:%S').replace(
                     tzinfo=pytz.utc)) > timedelta(1):
-                news.created_at = datetime.utcfromtimestamp(item['date_published']).replace(tzinfo=pytz.utc)
+                news.created_at = datetime.strptime(item['publishedAt'][:-5], '%Y-%m-%dT%H:%M:%S').replace(tzinfo=pytz.utc)
                 record.read = False
         if item['featured']:
             record.should_notify = True
@@ -75,7 +77,8 @@ def save_news(item):
             record.should_notify = False
 
         found = False
-        for event_id in item['events']:
+        for _event in item['events']:
+            event_id = _event['id']
             for event in news.events.all():
                 if event.id == event_id:
                     found = True
@@ -88,9 +91,10 @@ def save_news(item):
                     logger.error("No event found with ID %s" % event_id)
 
         found = False
-        for launch_id in item['launches']:
+        for _launch in item['launches']:
+            launch_id = _launch['id']
             for launch in news.launches.all():
-                if launch.id == launch_id:
+                if launch.id == launch_id['id']:
                     found = True
 
             if not found:
@@ -101,6 +105,6 @@ def save_news(item):
                     logger.error("No launch found with ID %s" % launch_id)
 
         news.link = item['url']
-        news.featured_image = item['featured_image']
+        news.featured_image = item['imageUrl']
         news.save()
         record.save()
