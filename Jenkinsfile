9a6ee29f77ddd55735b74da0d4b71ccd71929a4d
Caleb Jones <ca.jones9119@gmail.com>
2022-02-28 09:54:31 -0500

    Feature/k8s - lets do it (#200)
    * Change build flow to be multi-staged.
    
    * Update Jenkinsfile
    
    * Fix syntax
    
    * Adjust CMD
    
    * Add healthcheck endpoint
    
    * Adjust env detection of bools.
    
    * WIP k8s work
    
    * WIP update git URL
    
    * Revert ENV changes.
    
    * Revert back to python 3.6
    
    * Revert Jenkinsfile changes.
    
    * Update Jenkinsfile
    
    * Update Jenkinsfile
    
    * Update Dockerfile
    
    * Update dockerignore to include static dir.
    
    * Move code into src directory
    
    * Attempt to deploy helm via Jenkins
    
    * Fix /src related issue
    
    * Fix another /src reference
    
    * move helm chart into repo
    
    * Fix formatting issue in Jenksfile
    
    * Fix src syntax
    
    * Update Jenkinsfile
    
    * Change file path syntax
    
    * use unique helm release name
    
    * This is the one.
    
    * Implement sorting, flake8, and black hooks.
    
    * use imperative instead of declarative hpa
    
    * Remove hpa and pdb
    
    * Revert "use imperative instead of declarative hpa"
    
    This reverts commit f28c1d22cda4b17276b074acbbc7a21535c35ef1.
    
    * Revert "Remove hpa and pdb"
    
    This reverts commit a699a718b4c91d02a2d34271056ada8b9bf5df87.
    
    * Revert stupid issues, try again.
    
    * Revert "Implement sorting, flake8, and black hooks."
    
    This reverts commit 9e478efdb401a5afa731a0c322e853bf5281a739.
    
    * revert silliness
    
    * lets yeet this

--- Jenkinsfile
+++ Jenkinsfile
@@ -55,32 +55,23 @@ pipeline{
         stage('Setup'){
 			steps {
 				withCredentials([file(credentialsId: 'SLNTestConfig', variable: 'configFile')]) {
-					sh 'cp $configFile spacelaunchnow/config.py'
+					sh 'cp $configFile src/spacelaunchnow/config.py'
 				}
-				sh 'mkdir -p log'
-				sh 'touch log/daily_digest.log'
+				sh 'mkdir -p src/log'
+				sh 'touch src/log/daily_digest.log'
 				withPythonEnv('python3') {
-					sh 'python3 -m pip install -r requirements.txt'
+					sh 'python3 -m pip install -r src/requirements.txt'
 				}
 			}
 		}
-		stage('Tests'){
-			parallel {
-				stage('Run Django Tests'){
-					steps {
-						withPythonEnv('python3') {
-							sh 'python3 manage.py test'
-						}
-					}
-				}
-				stage('Run Formatting Checks'){
-					steps {
-						catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-							sh 'pylint **/*.py'
-						}
-					}
-				}
-			}
+		stage('Run Django Test'){
+            steps {
+                withPythonEnv('python3') {
+                    dir ("src") {
+                        sh 'python3 manage.py test'
+                    }
+                }
+            }
 		}
 		stage('Build Docker Image'){
 
@@ -88,11 +79,11 @@ pipeline{
 				script{
                     if (env.BRANCH_NAME == 'master') {
                         withCredentials([file(credentialsId: 'SLNProductionConfig', variable: 'configFile')]) {
-                            sh 'cp $configFile spacelaunchnow/config.py'
+                            sh 'cp $configFile src/spacelaunchnow/config.py'
                         }
                     } else {
                         withCredentials([file(credentialsId: 'SLNConfig', variable: 'configFile')]) {
-                            sh 'cp $configFile spacelaunchnow/config.py'
+                            sh 'cp $configFile src/spacelaunchnow/config.py'
                         }
                     }
 					if(!fileExists("Dockerfile")){
@@ -115,6 +106,8 @@ pipeline{
 						if (env.BRANCH_NAME == 'master') {
 						    dockerImage.push("${dockerTag}")
 						    dockerImage.push("production")
+						} else {
+                            dockerImage.push("${dockerTag}")
 						}
 						sh "docker ps -f name=" + branchName +" -q | xargs --no-run-if-empty docker container stop"
 						sh "docker run --rm -d --name sln-staging-" + imageName + " -p :8000 --network=web -l traefik.backend=sln-staging-" + imageName +" -l traefik.frontend.rule=Host:" + imageName + "-staging.calebjones.dev -l traefik.docker.network=web -l traefik.port=8000 " + registry + ":" + imageName + " 'bash' '-c' 'python /code/manage.py runserver 0.0.0.0:8000'"
@@ -123,10 +116,34 @@ pipeline{
 						if (env.BRANCH_NAME == 'master') {
 						    sh "docker tag ${registry}:production ${doRegistry}/sln-server:production"
 						    sh "docker push ${doRegistry}/sln-server:production"
+						} else {
+                            sh "docker tag ${registry}:${dockerTag} ${doRegistry}/sln-server:${dockerTag}"
+						    sh "docker push ${doRegistry}/sln-server:${dockerTag}"
 						}
 					}
 				}
 			}
+		}
+
+		stage('Deploy Helm Release'){
+		    steps {
+		        script {
+		            if (env.BRANCH_NAME == 'master') {
+		                sh '''
+		                    kubectl config use-context do-nyc1-k8s-spacelaunchnow-dev
+		                    export STAGING_NAMESPACE=sln-prod
+		                    export RELEASE_NAME=sln-prod-app
+		                    export DEPLOYS=$(helm ls --all-namespaces | grep $RELEASE_NAME | wc -l)
+		                    if [ $DEPLOYS  -eq 0 ];
+                            then
+		                        helm install $RELEASE_NAME k8s/helm/ --namespace=$STAGING_NAMESPACE --values k8s/helm/values.yaml;
+		                    else
+		                        helm upgrade $RELEASE_NAME k8s/helm/ --namespace=$STAGING_NAMESPACE --values k8s/helm/values.yaml;
+		                    fi
+		                '''
+		            }
+		        }
+		    }
 		}
     }
     post {
@@ -140,9 +157,9 @@ pipeline{
                 webhookURL: DISCORD_URL,
                 thumbnail: "https://i.imgur.com/FASV6fJ.png",
                 notes: "Hey <@&641718676046872588>, new build completed for ${PROJECT_NAME}!"
-            // This needs to be removed in favor or removing credential files instead.
+            // This needs to be removed in favor of removing credential files instead.
             sh '''
-               rm spacelaunchnow/config.py
+               rm src/spacelaunchnow/config.py
                '''
         }
     }
---
+++ src/bot/tasks.py
@@ -0,0 +1,213 @@
+# coding=utf-8
+import json
+
+import requests
+from datetime import timedelta
+
+from celery import Celery
+
+from bot.app.digest.digest import DigestServer
+from celery.task import periodic_task
+from celery.utils.log import get_task_logger
+
+from bot.app.events.event_tracker import EventTracker
+
+from bot.app.notifications.launch_event_tracker import LaunchEventTracker
+from bot.app.sync.closure_sync import get_road_closure
+from bot.app.sync.reddit_sync import get_submissions
+from bot.app.sync.twitter_sync import get_new_tweets
+from bot.app.sync.news_sync import get_news
+from celery.schedules import crontab
+
+logger = get_task_logger('tasks')
+
+TAG = 'Digest Server'
+
+app = Celery()
+
+
+@periodic_task(
+    run_every=(crontab(minute=0, hour=12,
+                       day_of_week='mon-sun')),
+    name="run_daily",
+    ignore_result=True,
+    options={"expires": 3600}
+)
+def run_daily():
+    logger.info('Task - Running Digest - Daily...')
+    daily_digest = DigestServer()
+    daily_digest.run(daily=True)
+
+
+# @periodic_task(
+#     run_every=(crontab(minute=0, hour=12,
+#                        day_of_week='mon-sun')),
+#     name="run_daily_cleanup",
+#     ignore_result=True,
+#     options={"expires": 3600}
+# )
+# def run_daily(send_webhook=True):
+#     logger.info('Task - Running Digest - Daily Cleanup...')
+#     data = check_for_orphaned_launches(send_webhook=send_webhook)
+#     return data
+
+
+@periodic_task(
+    run_every=(crontab(minute=0, hour=12,
+                       day_of_week='mon')),
+    name="run_weekly",
+    ignore_result=True,
+    options={"expires": 3600}
+)
+def run_weekly():
+    logger.info('Task - Running Digest - Weekly...')
+    daily_digest = DigestServer()
+    daily_digest.run(weekly=True)
+
+
+# @periodic_task(run_every=(crontab(hour='*/2')), options={"expires": 15})
+# def get_upcoming_launches():
+#     logger.info('Task - Get Upcoming launches - every two hours!')
+#     repository = LaunchRepository()
+#     repository.get_next_launches(next_count=100, all=True)
+
+
+@periodic_task(run_every=(crontab(hour='*/2')), options={"expires": 15})
+def get_road_closures():
+    logger.info('Task - Get Road Closures!')
+    get_road_closure()
+
+
+# def check_for_orphaned_launches(send_webhook=True):
+#     logger.info('Task - Get Upcoming launches!')
+#
+#     # Delete notification records from old launches.
+#     seven_days_threshhold = datetime.today() - timedelta(days=7)
+#     thirty_days_threshhold = datetime.today() - timedelta(days=30)
+#
+#     notifications = LaunchNotificationRecord.objects.filter(launch__net__lte=seven_days_threshhold)
+#     notifications.delete()
+#
+#     # TODO only delete if the submission is not stickied.
+#     # submissions = RedditSubmission.objects.filter(created_at__lte=thirty_days_threshhold)
+#     # submissions.delete()
+#
+#     tweet = Tweet.objects.filter(created_at__lte=thirty_days_threshhold)
+#     tweet.delete()
+#
+#     news = ArticleNotification.objects.filter(created_at__lte=thirty_days_threshhold)
+#     news.delete()
+#
+#     # Check for stale launches.
+#     three_days_past = datetime.today() - timedelta(days=3)
+#     launches = Launch.objects.filter(last_updated__lte=three_days_past, launch_library=True)
+#     count = 0
+#     stale = []
+#     if len(launches) > 0:
+#         logger.info("Found stale launches - checking to see if they are deleted from Launch Library")
+#         repository = LaunchRepository()
+#         for launch in launches:
+#             logger.debug("Stale - %s" % launch.name)
+#             if repository.is_launch_deleted(launch.launch_library_id):
+#                 stale.append(launch)
+#                 logger.error("Delete this launch! - %s ID: %d" % (launch.name, launch.launch_library_id))
+#
+#     url = "https://discordapp.com/api/webhooks/681358922774478868/XtzAAHnbE8X930eeXFEwL1bGll-ucFD0xKMDwXUWdHuHvZW8qcqQOYO1eq9Mpyryl2zL"
+#     data = {}
+#     data["content"] = "**Daily Stale Checker**"
+#     data["username"] = "Space Launch Bot"
+#
+#     # leave this out if you dont want an embed
+#     data["embeds"] = []
+#     embed = {}
+#     description = ""
+#     title = "Found %s stale launches..." % len(stale)
+#     for launch in stale:
+#         description = description + "%s\n [Launch Library](https://launchlibrary.net/1.4/launch/%s) | [Admin](%s)\n\n" % (
+#             launch.name,
+#             launch.launch_library_id,
+#             launch.get_admin_url())
+#     # for all params, see https://discordapp.com/developers/docs/resources/channel#embed-object
+#     embed["description"] = (description[:2000] + '\n ... too many stale to show.') if len(description) > 2000 else description
+#     embed["title"] = title
+#     data["embeds"].append(embed)
+#     logger.info(data)
+#
+#     if send_webhook or len(stale) > 5:
+#         result = requests.post(url, data=json.dumps(data), headers={"Content-Type": "application/json"})
+#         try:
+#             result.raise_for_status()
+#         except requests.exceptions.HTTPError as err:
+#             logger.error(err)
+#         else:
+#             logger.info("Payload delivered successfully, code {}.".format(result.status_code))
+#     else:
+#         logger.info(data)
+#         return data
+
+
+# @periodic_task(
+#     run_every=(crontab(minute=0, hour=3,
+#                        day_of_week='mon-sun')),
+#     name="get_previous",
+#     ignore_result=True,
+#     options={"expires": 3600}
+# )
+# def get_previous_launches():
+#     logger.info('Task - Get Previous launches!')
+#     repository = LaunchRepository()
+#     repository.get_previous_launches()
+#     logger.info('Task - Getting SpaceX cores!')
+
+
+# @periodic_task(run_every=(crontab(minute='*/1')), options={"expires": 60})
+# def check_next_launch(debug=config.DEBUG):
+#     logger.info('Task - Running Notifications...')
+#     notification = LaunchLibrarySync(debug=debug)
+#     notification.check_next_launch()
+
+
+@periodic_task(run_every=(crontab(minute='*/5')), options={"expires": 60})
+def get_tweets_task():
+    logger.info('Task - Running get_new_tweets...')
+    get_new_tweets()
+
+
+@periodic_task(run_every=(crontab(minute='*/5')), options={"expires": 120})
+def get_news_task():
+    logger.info('Task - Running get_news...')
+    get_news()
+
+
+@periodic_task(run_every=(crontab(hour='*/1')), options={"expires": 120})
+def get_news_task_hourly():
+    logger.info('Task - Running get_news...')
+    get_news(limit=50)
+
+
+@periodic_task(run_every=(crontab(minute='*/30')), options={"expires": 120})
+def get_reddit_submissions_task():
+    logger.info('Task - Running get_reddit_submissions...')
+    get_submissions()
+
+
+@periodic_task(run_every=timedelta(seconds=15), options={"expires": 5})
+def launch_tracker():
+    logger.info('Task - Running Launch Event Tracker')
+    tracker = LaunchEventTracker()
+    tracker.check_events()
+
+
+@periodic_task(run_every=timedelta(seconds=60), options={"expires": 5})
+def event_tracker():
+    logger.info('Task - Running Event Tracker')
+    tracker = EventTracker()
+    tracker.check_events()
+    tracker.check_news_item()
+
+
+# @periodic_task(run_every=(crontab(minute='*/5')), options={"expires": 60})
+# def get_recent_previous_launches():
+#     logger.info('Task - Get Recent Previous launches!')
+#     repository = LaunchRepository()
+#     repository.get_recent_previous_launches()
