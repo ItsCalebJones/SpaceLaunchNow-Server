09289068465c462557649172792ab0f41f833028
Caleb Jones <ca.jones9119@gmail.com>
2022-05-13 21:33:04 -0400

    feat: enable notification server (#208)

--- src/bot/management/commands/run_notification_service.py
+++ src/bot/management/commands/run_notification_service.py
@@ -1,23 +1,33 @@
 import logging
+import os
 
 import time
 
 from django.core.management import BaseCommand
+
+from bot.app.events.event_tracker import EventTracker
+from bot.app.notifications.launch_event_tracker import LaunchEventTracker
 
 logger = logging.getLogger(__name__)
 
 TAG = 'Notification Service'
 
 
-def run():
-    logger.info("Starting health check...")
-    while True:
-        logger.info("Health check!")
-        time.sleep(30)
-
-
 class Command(BaseCommand):
     help = 'Runs the notification service.'
 
     def handle(self, *args, **options):
-        run()
+        self._run()
+
+    def _run(self):
+        self.launch_tracker = LaunchEventTracker()
+        self.event_tracker = EventTracker()
+        logger.info("Starting health check...")
+
+        while True:
+            logger.info("Health check!")
+            self.launch_tracker.check_events()
+
+            self.event_tracker.check_events()
+            self.event_tracker.check_news_item()
+            time.sleep(30)
