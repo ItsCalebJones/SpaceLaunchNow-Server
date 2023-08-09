2f452bc91a9f7a9003da7adb8c70b751d99eddcc
Jay <jay_keegan@icloud.com>
2023-08-09 23:30:40 +0100

    fix: Fixed a number of issues with closure scraper

--- src/bot/app/sync/closure_sync.py
+++ src/bot/app/sync/closure_sync.py
@@ -18,16 +18,10 @@ user_agent = (
 
 
 def parse_date(date_string):
-    tzTex = pytz.timezone("US/Central")
-    try:
-        datetime = tzTex.localize(parser.parse(date_string))
-    except Exception:
-        datetime = tzTex.localize(parser.parse(date_string.split(", ", 1)[1]))
-    return datetime
+    return datetime.strptime(date_string, "%b %d, %Y %I:%M %p").replace(tzinfo=pytz.timezone("US/Central"))
 
 
 def get_road_closure():
-
     url = "https://www.cameroncountytx.gov/spacex/"
     request = urllib.request.Request(url, headers={"User-Agent": user_agent})
     try:
@@ -41,32 +35,37 @@ def get_road_closure():
     html_content = response.read()
 
     # Parse the html content
-    soup = BeautifulSoup(html_content, "lxml")
-    tableRows = []
-    gdp_table = soup.find_all("tbody")
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    gdp_table = soup.select("#vc_row-64d3ad6dcf9ba > div > div > div > div > table > tbody:nth-child(3)")
+    
     gdp_table_data = []
+    closures = []
+
     for table in gdp_table:
-        gdp_table_data.append(table.find_all("tr"))  # contains 2 rows
+        gdp_table_data.append(table.find_all("tr"))
     for table in gdp_table_data:
         for row in table:
-            aux = ""
-            for cell in row.find_all("td"):
-                aux += str(cell.text.strip()) + "|"
-            tableRows.append(aux[:-1])
+            # get each td
+            cols = row.find_all("td")
 
-    closures = []
-    for row in tableRows:
-        try:
-            if len(row) > 3:
-                row = row.replace("a.m.", "AM")
-                row = row.replace("p.m.", "PM")
+            type_col = cols[0].text.strip()
+            date_col = cols[1].text.strip()
+            time_col = cols[2].text.strip()
+            status_col = cols[3].text.strip()
 
-                start_date = row.split("|")[1]
-                closure_time = row.split("|")[2]
-                start_time = closure_time.split(" to ")[0].strip()
-                closure_end = closure_time.split(" to ")[1].strip()
+            if type_col == "" or date_col == "" or time_col == "" or status_col == "":
+                continue
+            
+            try:
+                time_col = time_col.replace(".", "")
+                time_col = time_col.replace("am", "AM")
+                time_col = time_col.replace("pm", "PM")
+                
+                start_time = time_col.split(" to ")[0].strip()
+                closure_end = time_col.split(" to ")[1].strip()
 
-                start_string = start_date + " " + start_time
+                start_string = date_col + " " + start_time
                 start_datetime = parse_date(start_string)
 
                 now = datetime.now(tz=pytz.utc)
@@ -78,23 +77,22 @@ def get_road_closure():
                     end_date = datetime.strptime(end_date, "%B %d").replace(year=now.year).strftime("%A, %B %d, %Y")
                     end_string = end_date + " " + end_time
                 else:
-                    end_string = start_date + " " + closure_end
+                    end_string = date_col + " " + closure_end
                 end_datetime = parse_date(end_string)
                 if end_datetime < start_datetime:
                     end_datetime += timedelta(days=1)
-                print(f"start: {start_datetime} end: {end_datetime}")
 
                 if end_datetime < now:
                     print("skipping as end date is in the past")
                     continue
 
-                name = row.split("|")[0]
-                status = row.split("|")[3].replace("Closure", "").strip()
+                name = type_col
+                status = status_col
                 closures.append([start_datetime, end_datetime, name, status])
-        except Exception as e:
-            logger.error(row)
-            logger.error(e)
-            continue
+            except Exception as e:
+                logger.error(row)
+                logger.error(e)
+                continue
 
     logger.info("Found %s closures" % len(closures))
     for closure in closures:
