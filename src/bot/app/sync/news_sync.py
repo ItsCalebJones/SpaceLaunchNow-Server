20506ed5320b2f22fa5dd8710b7b2286118e37a4
Caleb Jones <ca.jones9119@gmail.com>
2023-10-16 09:21:09 -0400

    feat: migrate to snapi v4

--- src/bot/app/sync/news_sync.py
+++ src/bot/app/sync/news_sync.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import pytz
 import requests
@@ -11,34 +11,34 @@ logger = logging.getLogger(__name__)
 
 
 def get_news(limit=10):
-    response = requests.get(url="https://api.spaceflightnewsapi.net/v3/articles?_limit=%s" % limit)
+    response = requests.get(url=f"https://api.spaceflightnewsapi.net/v4/articles/?limit={limit}")
     if response.status_code == 200:
         articles = response.json()
         logger.info("Found %s articles." % len(articles))
-        for item in articles:
+        for item in articles['results']:
             save_news_LL(item)
 
 
 def get_related_news():
     launches = Launch.objects.all()
     for launch in launches:
-        url = f"https://api.spaceflightnewsapi.net/v3/articles/launch/{launch.id}"
+        url = f"https://api.spaceflightnewsapi.net/v4/articles/?launch={launch.id}"
         logger.debug(f"Looking for related articles - {url}")
         response = requests.get(url=url)
         if response.status_code == 200:
             data = response.json()
-            logger.info(f"Found {len(data)} articles.")
-            for item in data:
+            logger.info(f"Found {data['count']} articles.")
+            for item in data['results']:
                 save_news_LL(item)
     events = Events.objects.all()
     for event in events:
-        url = f"https://api.spaceflightnewsapi.net/v3/articles/event/{event.id}"
+        url = f"https://api.spaceflightnewsapi.net/v4/articles/?event={event.id}"
         logger.debug(f"Looking for related articles - {url}")
         response = requests.get(url=url)
         if response.status_code == 200:
             data = response.json()
-            logger.info(f"Found {len(data)} articles.")
-            for item in data:
+            logger.info(f"Found {data['count']} articles.")
+            for item in data['results']:
                 save_news_LL(item)
 
 
@@ -49,19 +49,19 @@ def save_news_LL(item):
         logger.debug(f"Creating article for article id {item['id']}.")
         news.title = item["title"]
         news.link = item["url"]
-        news.featured_image = item["imageUrl"]
-        news.news_site = item["newsSite"]
+        news.featured_image = item["image_url"]
+        news.news_site = item["news_site"]
         news.description = item["summary"]
-        news.created_at = datetime.strptime(item["publishedAt"][:-5], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=pytz.utc)
+        news.created_at = datetime.strptime(item["published_at"], "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
         for _event in item["events"]:
-            event_id = _event["id"]
+            event_id = _event["event_id"]
             try:
                 event = Events.objects.get(id=event_id)
                 news.events.add(event)
             except Events.DoesNotExist:
                 logger.error("No event found with ID %s" % event_id)
         for _launch in item["launches"]:
-            launch_id = _launch["id"]
+            launch_id = _launch["launch_id"]
             try:
                 launch = Launch.objects.get(id=launch_id)
                 news.launches.add(launch)
@@ -81,13 +81,11 @@ def save_news_LL(item):
         logger.info(f"Updating article for article id {item['id']} | {news.id} and record id {record.id}.")
         if news.title != item["title"]:
             news.title = item["title"]
-            if (
-                news.created_at
-                - datetime.strptime(item["publishedAt"][:-5], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=pytz.utc)
-            ) > timedelta(1):
-                news.created_at = datetime.strptime(item["publishedAt"][:-5], "%Y-%m-%dT%H:%M:%S").replace(
-                    tzinfo=pytz.utc
-                )
+            item_date_str = item["published_at"].replace("Z", "+00:00")
+            item_date = datetime.fromisoformat(item_date_str).replace(tzinfo=timezone.utc)
+            if news.created_at - item_date > timedelta(1):
+                news.created_at = item_date
+                logger.info("")
                 record.read = True
                 record.was_notified = True
 
@@ -98,7 +96,7 @@ def save_news_LL(item):
 
         found = False
         for _event in item["events"]:
-            event_id = _event["id"]
+            event_id = _event["event_id"]
             for event in news.events.all():
                 if event.id == event_id:
                     found = True
@@ -112,7 +110,7 @@ def save_news_LL(item):
 
         found = False
         for _launch in item["launches"]:
-            launch_id = _launch["id"]
+            launch_id = _launch["launch_id"]
             for launch in news.launches.all():
                 if launch.id == launch_id:
                     found = True
@@ -124,10 +122,10 @@ def save_news_LL(item):
                 except Launch.DoesNotExist:
                     logger.error("No launch found with ID %s" % launch_id)
 
-        news.news_site = item["newsSite"]
+        news.news_site = item["news_site"]
         news.description = item["summary"]
         news.link = item["url"]
-        news.featured_image = item["imageUrl"]
+        news.featured_image = item["image_url"]
         news.save()
         record.save()
         logger.info(f"Article saved for article id {item['id']}.")
